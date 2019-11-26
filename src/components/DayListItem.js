61e734a5f9cd9e939e1994f6df265b008ad95a0e
Ted Ogmundson <togmund@gmail.com>
2019-11-25 16:04:15 -0800

    updated daylistitem tests

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -7,18 +7,23 @@ import classNames from 'classnames';
 export default function DayListItem({ name, spots, selected, setDay }) {
 
   const dayClass = classNames("day-list__item", {
-    'day-list__item--selected':selected,
-    'day-list__item--full': spots = 0
+    'day-list__item--selected': selected,
+    'day-list__item--full': spots === 0
   })
+  const formattedSpots =
+    spots > 1
+      ? spots + ' spots remaining'
+      : spots === 1
+        ? spots + ' spot remaining'
+        : 'no spots remaining'
 
   return (
-    
     <li
       className={dayClass}
       onClick={() => setDay(name)}
-      >
+    >
       <h2>{name}</h2>
-      <h3 >{spots}</h3>
+      <h3 >{formattedSpots}</h3>
     </li>
   );
 }
