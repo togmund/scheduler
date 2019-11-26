80d9c30ceef45b049685ba0822357187efccb53e
Ted Ogmundson <togmund@gmail.com>
2019-11-26 15:08:22 -0800

    updated the scaffolding for the Form Button, filling with props

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,18 +1,49 @@
 import React from "react";
+import Button from "components/Button"
+import InterviewerList from "components/InterviewerList"
 
 // import classNames from 'classnames';
 
-export default function Empty({onAdd}) {
+export default function Empty({name, interviewers, currentInterviewer, onSave, onCancel}) {
   // const buttonClass = classNames();
 
   return (
-    <main className="appointment__add">
-      <img
-        className="appointment__add-button"
-        src="images/add.png"
-        alt="Add"
-        onClick={onAdd}
+<main className="appointment__card appointment__card--create">
+  <section className="appointment__card-left">
+    <form autoComplete="off">
+      <input
+        className="appointment__create-input text--semi-bold"
+        name="name"
+        type="text"
+        placeholder="Enter Student Name"
+        /*
+          This must be a controlled component
+        */
       />
-    </main>
+    </form>
+    <InterviewerList interviewers={interviewers} value={currentInterviewer} onChange={setInterviewer} />
+  </section>
+  <section className="appointment__card-right">
+    <section className="appointment__actions">
+      <Button danger onClick={onCancel}>Cancel</Button>
+      <Button confirm onClick={onSave}>Save</Button>
+    </section>
+  </section>
+</main>
   );
 }
+
+// The Form component should track the following state:
+// name:String
+// interviewer:Number
+
+// The Form component should have the following actions:
+// setName:Function
+// setInterviewer:Function
+
+// The Form component should take the following props:
+// name:String
+// interviewers:Array
+// interviewer:Number
+// onSave:Function
+// onCancel:Function
