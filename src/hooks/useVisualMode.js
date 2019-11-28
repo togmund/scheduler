c8af9c4ce7f12d5dedb366b79496f6c8722cd447
Ted Ogmundson <togmund@gmail.com>
2019-11-27 18:28:57 -0800

    set limit to back function, will only run if there are more than one entry

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -10,8 +10,10 @@ export default function useVisualMode(initial) {
   }
 
   const back = () => {
-    history.pop()
-    setMode(history[history.length-1]);
+    if (history.length > 1) {
+      history.pop()
+      setMode(history[history.length-1]);
+    }
   }
 
   return { mode, transition, back };
