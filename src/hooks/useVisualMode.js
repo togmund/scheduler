eb140df54f1d9ef058098c6dcf802c4c2d7214b0
Ted Ogmundson <togmund@gmail.com>
2019-11-27 17:15:09 -0800

    implemented mode and transition, still fiddling with history and back

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,3 +1,16 @@
-export default function useVisualMode (mode) {
-  return mode;
-}
+import React, { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  const transition = (newMode) => {
+    setMode(newMode);
+    setHistory(newMode);
+  }
+  const back = () => {
+    setMode(history);
+  }
+
+  return { mode, transition, back };
+};
