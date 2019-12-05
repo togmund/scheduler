49deec37ac9f9d91217d6bd0a1c8dfb310dc2cdb
togmund <togmund@gmail.com>
2019-12-04 18:40:04 -0800

    book and edit interview tests working, now doing cancel test

---
+++ cypress/integration/appointments.spec.js
@@ -0,0 +1,39 @@
+describe("Appointments", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/debug/reset");
+    cy.visit("/");
+    cy.contains("Monday");
+  });
+  it("should book an interview", () => {
+    cy.get("[alt=Add]").as("addButton");
+    cy.get("@addButton")
+      .first()
+      .click();
+    cy.get("[data-testid=student-name-input]").type("Frank Rose");
+    cy.get("[alt='Sylvia Palmer']").click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Frank Rose");
+    cy.contains(".appointment__card--show", "Sylvia Palmer");
+  });
+  it("should edit an interview", () => {
+    cy.get("[alt=Edit]").as("editButton");
+    cy.get("@editButton")
+      .invoke("show")
+      .click();
+    cy.get("[data-testid=student-name-input]")
+      .clear()
+      .type("Frank Rose");
+    cy.get("[alt='Tori Malcolm']").click();
+    cy.contains("Save").click();
+    cy.contains(".appointment__card--show", "Frank Rose");
+    cy.contains(".appointment__card--show", "Tori Malcolm");
+  });
+  it("should cancel an interview", () => {
+    cy.get("[alt=Delete]").as("deleteButton");
+    cy.get("@deleteButton")
+      .invoke("show")
+      .click();
+    cy.get("[alt=Confrim]").as("confirmButton");
+    cy.get("@@confirmButton").click();
+  });
+});
