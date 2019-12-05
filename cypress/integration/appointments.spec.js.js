7279c9ba5a00866f0f5dad5b21319fa6fdb17515
togmund <togmund@gmail.com>
2019-12-04 17:04:47 -0800

    created file for ans scaffoleded out the book, edit and cancel interview cypress tests

---
+++ cypress/integration/appointments.spec.js.js
@@ -0,0 +1,11 @@
+describe("Appointments", () => {
+  it("should book an interview", () => {
+    cy.visit("/");
+  });
+  it("should edit an interview", () => {
+    cy.visit("/");
+  });
+  it("should cancel an interview", () => {
+    cy.visit("/");
+  });
+});
